35c82e219db39c22cb480216f1e5eb6a0f14d25f
CAPurtic <68652544+CAPurtic@users.noreply.github.com>
2021-03-27 21:10:29 -0600

    Removed and Added
    Removed feature frequencies and changed name of convert_to_bool function to convert_to_int

--- online_shoppers_script.py
+++ online_shoppers_script.py
@@ -12,16 +12,6 @@ def generate_EDA_report(df, report_title="EDA_report",
     profile.to_file(file_name)
 
 
-def feature_frequencies(feature):
-    """Check a column for value balance."""
-    try:
-        return feature.value_counts(normalize=True)
-    except TypeError:
-        print("Error! Unable to compute frequencies for this variable type.")
-    except AttributeError:
-        print("Error! This object does not have the attribute 'value_counts'.")
-
-
 def generate_dummy_columns(df, columns_to_dummy: list,
                            drop_in_place: bool = True):
     """Replaces given columns with dummy columns."""
@@ -29,10 +19,10 @@ def generate_dummy_columns(df, columns_to_dummy: list,
     return df
 
 
-def generate_bool_columns(df, columns_to_bool: list):
+def generate_int_columns(df, columns_to_convert_to_int: list):
     """Convert a given list of columns to type int."""
     try:
-        df[columns_to_bool] = df[columns_to_bool].astype(int)
+        df[columns_to_convert_to_int] = df[columns_to_convert_to_int].astype(int)
         return df
     except ValueError:
         print("Error! Unable to convert a column to int.")
@@ -43,9 +33,6 @@ convert_to_dummy = ["Month", "VisitorType"]
 #                                         False)
 
 convert_to_bool = ["Weekend", "Revenue", "VisitorType"]
-online_shoppers = generate_bool_columns(online_shoppers, convert_to_bool)
+online_shoppers = generate_int_columns(online_shoppers, convert_to_bool)
 
 
-print(online_shoppers["Weekend"].head())
-print(online_shoppers["Revenue"].head())
-print(feature_frequencies(online_shoppers["Revenue"]))

279807ab8059bbde3171576849eb7b370c5d3ff7
CAPurtic <68652544+CAPurtic@users.noreply.github.com>
2021-03-28 22:27:54 -0600

    random forest function added

--- online_shoppers_script.py
+++ online_shoppers_script.py
@@ -50,6 +50,26 @@ def upsample(X_df, y_df):
     return X_df_oversampled, y_df_oversampled
 
 
+def create_random_forest(
+    X_trn, X_tst, y_trn, y_tst, min_data_points_percent=0.05, estimators=100
+):
+    min_data_points_per_leaf = int(
+        round(len(X_train_oversampled) * min_data_points_percent, 0)
+    )
+    clf = RandomForestClassifier(
+        n_estimators=estimators, min_samples_leaf=min_data_points_per_leaf
+    )
+    clf.fit(X_trn, y_trn)
+    y_predictions = clf.predict(X_tst)
+
+    clf_accuracy = round(metrics.accuracy_score(y_tst, y_predictions) * 100, 2)
+    print(f"Overall weighted model accuracy is: {clf_accuracy}%\n")
+
+    clf_confusion_matrix = metrics.confusion_matrix(y_tst, y_predictions)
+    clf_confusion_matrix_plot = metrics.plot_confusion_matrix(clf, X_tst, y_tst)
+    print(metrics.classification_report(y_tst, y_predictions))
+
+
 convert_to_dummy = ["Month", "VisitorType"]
 online_shoppers = generate_dummy_columns(online_shoppers, convert_to_dummy, False)
 
@@ -65,20 +85,4 @@ y_test = split_sets["y_test_set"]
 
 X_train_oversampled, y_train_oversampled = upsample(X_train, y_train)
 
-min_data_points_per_leaf = int(round(len(X_train_oversampled) * 0.05, 0))
-clf = RandomForestClassifier(
-    n_estimators=100, min_samples_leaf=min_data_points_per_leaf
-)
-
-clf.fit(X_train_oversampled, y_train_oversampled)
-
-y_pred = clf.predict(X_test)
-
-clf_accuracy = round(metrics.accuracy_score(y_test, y_pred) * 100, 2)
-# print(f"Overall weighted model accuracy is: {clf_accuracy}%\n")
-
-# Confusion matrix
-clf_confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
-clf_confusion_matrix_plot = metrics.plot_confusion_matrix(clf, X_test, y_test)
-
-# print(metrics.classification_report(y_test, y_pred))
+create_random_forest(X_train_oversampled, X_test, y_train_oversampled, y_test)

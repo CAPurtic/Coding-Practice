f797da175eb00ee161400e3e70f64f10f007cf9f
CAPurtic <68652544+CAPurtic@users.noreply.github.com>
2021-03-28 21:50:04 -0600

    split_dataset function
    Converted the splitting of training/testing to a function.

--- online_shoppers_script.py
+++ online_shoppers_script.py
@@ -29,21 +29,38 @@ def generate_int_columns(df, columns_to_convert_to_int: list):
         print("Error! Unable to convert a column to int.")
 
 
+def split_dataset(df, dependent_column, test_set_size_percent=0.20):
+    X_train_set, X_test_set, y_train_set, y_test_set = train_test_split(
+        df.loc[:, df.columns != dependent_column],
+        df[dependent_column],
+        test_size=test_set_size_percent,
+    )
+    split_data = {
+        "X_train_set": X_train_set,
+        "X_test_set": X_test_set,
+        "y_train_set": y_train_set,
+        "y_test_set": y_test_set,
+    }
+    return split_data
+
+
 convert_to_dummy = ["Month", "VisitorType"]
 online_shoppers = generate_dummy_columns(online_shoppers, convert_to_dummy, False)
 
 convert_to_bool = ["Weekend", "Revenue"]
 online_shoppers = generate_int_columns(online_shoppers, convert_to_bool)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    online_shoppers.loc[:, online_shoppers.columns != "Revenue"],
-    online_shoppers["Revenue"],
-    test_size=0.20,
-)
+split_sets = split_dataset(online_shoppers, "Revenue", test_set_size_percent=0.20)
+
+X_train = split_sets["X_train_set"]
+X_test = split_sets["X_test_set"]
+y_train = split_sets["y_train_set"]
+y_test = split_sets["y_test_set"]
+
 
 sm = SMOTE()
-X_train_oversampled, y_train_oversampled = sm.fit_sample(
-    X_train, y_train.ravel())
+X_train_oversampled, y_train_oversampled = sm.fit_resample(X_train, y_train.ravel())
+
 
 min_data_points_per_leaf = int(round(len(X_train_oversampled) * 0.05, 0))
 clf = RandomForestClassifier(
@@ -54,11 +71,11 @@ clf.fit(X_train_oversampled, y_train_oversampled)
 
 y_pred = clf.predict(X_test)
 
-clf_accuracy = round(metrics.accuracy_score(y_test, y_pred) * 100,2)
-print(f'Overall weighted model accuracy is: {clf_accuracy}%\n')
+clf_accuracy = round(metrics.accuracy_score(y_test, y_pred) * 100, 2)
+# print(f"Overall weighted model accuracy is: {clf_accuracy}%\n")
 
 # Confusion matrix
 clf_confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
 clf_confusion_matrix_plot = metrics.plot_confusion_matrix(clf, X_test, y_test)
 
-print(metrics.classification_report(y_test, y_pred))
+# print(metrics.classification_report(y_test, y_pred))
